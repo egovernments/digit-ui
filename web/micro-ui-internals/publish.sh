b74a0a44aa0c7196b5f4b6a413790426c9b5a68a
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2023-07-25 12:11:01 +0530

    updated the publish script

--- web/micro-ui-internals/publish.sh
+++ web/micro-ui-internals/publish.sh
@@ -11,13 +11,13 @@ msg() {
 # sleep 5
 
 msg "Building and publishing css"
-cd "$BASEDIR/packages/css" && yarn publish --access public
+cd "$BASEDIR/packages/css" &&  rm -rf dist && yarn&& npm publish --tag fsm-1.3
 
 msg "Building and publishing libraries"
-cd "$BASEDIR/packages/libraries" && yarn publish --access public
+cd "$BASEDIR/packages/libraries" &&  rm -rf dist && yarn&& npm publish --tag fsm-1.3
 
 msg "Building and publishing react-components"
-cd "$BASEDIR/packages/react-components" && yarn publish --access public
+cd "$BASEDIR/packages/react-components" &&  rm -rf dist && yarn&& npm publish --tag fsm-1.3
 
 # sleep 10
 # msg "Updating dependencies"
@@ -25,19 +25,23 @@ cd "$BASEDIR/packages/react-components" && yarn publish --access public
 # sleep 5
 
 msg "Building and publishing PGR module"
-cd "$BASEDIR/packages/modules/pgr" && yarn publish --access public
+cd "$BASEDIR/packages/modules/pgr" &&  rm -rf dist && yarn&& npm publish --tag fsm-1.3
 
 msg "Building and publishing FSM module"
-cd "$BASEDIR/packages/modules/fsm" && yarn publish --access public
+cd "$BASEDIR/packages/modules/fsm" &&  rm -rf dist && yarn&& npm publish --tag fsm-1.3
 
 msg "Building and publishing PT module"
-cd "$BASEDIR/packages/modules/pt" && yarn publish --access public
+cd "$BASEDIR/packages/modules/pt" &&  rm -rf dist && yarn&& npm publish --tag fsm-1.3
 
 msg "Building and publishing DSS module"
-cd "$BASEDIR/packages/modules/dss" && yarn publish --access public
+cd "$BASEDIR/packages/modules/dss" &&  rm -rf dist && yarn&& npm publish --tag fsm-1.3
 
 msg "Building and publishing Common module"
-cd "$BASEDIR/packages/modules/common" && yarn publish --access public
+cd "$BASEDIR/packages/modules/common" &&  rm -rf dist && yarn&& npm publish --tag fsm-1.3
+
+
+msg "Building and publishing engagement module"
+cd "$BASEDIR/packages/modules/engagement" &&  rm -rf dist && yarn&& npm publish --tag fsm-1.3
 
 msg "Building and publishing Core module"
-cd "$BASEDIR/packages/modules/core" && yarn publish --access public
+cd "$BASEDIR/packages/modules/core" &&  rm -rf dist && yarn&& npm publish --tag fsm-1.3
