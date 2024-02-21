310acdec2bf1281e3b90f88e2d4106011206316d
Anil Singha <99383116+anilsingha-eGov@users.noreply.github.com>
2024-02-21 10:51:22 +0530

    SM-3861: Added TQM module

--- web/src/App.js
+++ web/src/App.js
@@ -1,53 +1,54 @@
-import React from "react";
+import React from 'react';
 
 import {
   initPGRComponents,
   PGRReducers,
-} from "@egovernments/digit-ui-module-pgr";
-import { initFSMComponents } from "@egovernments/digit-ui-module-fsm";
+} from '@egovernments/digit-ui-module-pgr';
+import { initFSMComponents } from '@egovernments/digit-ui-module-fsm';
 // import {
 //   PTModule,
 //   PTLinks,
 //   PTComponents,
 // } from "@egovernments/digit-ui-module-pt";
 // import { MCollectModule, MCollectLinks, initMCollectComponents } from "@egovernments/digit-ui-module-mcollect";
-import { initDSSComponents } from "@egovernments/digit-ui-module-dss";
+import { initDSSComponents } from '@egovernments/digit-ui-module-dss';
 import {
   PaymentModule,
   PaymentLinks,
   paymentConfigs,
-} from "@egovernments/digit-ui-module-common";
-import { DigitUI } from "@egovernments/digit-ui-module-core";
-import { initLibraries } from "@egovernments/digit-ui-libraries";
+} from '@egovernments/digit-ui-module-common';
+import { DigitUI } from '@egovernments/digit-ui-module-core';
+import { initLibraries } from '@egovernments/digit-ui-libraries';
 // import { HRMSModule ,initHRMSComponents  } from "@egovernments/digit-ui-module-hrms";
 // import { TLModule, TLLinks,initTLComponents } from "@egovernments/digit-ui-module-tl";
 // import { initReceiptsComponents, ReceiptsModule } from "@egovernments/digit-ui-module-receipts";
 // import { initOBPSComponents } from "@egovernments/digit-ui-module-obps";
 // import { initNOCComponents } from "@egovernments/digit-ui-module-noc";
-import { initEngagementComponents } from "@egovernments/digit-ui-module-engagement";
+import { initEngagementComponents } from '@egovernments/digit-ui-module-engagement';
 // import { initWSComponents } from "@egovernments/digit-ui-module-ws";
 // import { initCommonPTComponents } from "@egovernments/digit-ui-module-commonpt";
 
-import { customizations, initCustomisationComponents } from "./Customisations";
+import { customizations, initCustomisationComponents } from './Customisations';
 
 initLibraries();
 //"WS" removed the ws enabledModules ;
 const enabledModules = [
-  "PGR",
-  "FSM",
-  "Payment",
-  "PT",
-  "QuickPayLinks",
-  "DSS",
-  "NDSS",
-  "MCollect",
-  "HRMS",
-  "TL",
-  "Receipts",
-  "OBPS",
-  "NOC",
+  'PGR',
+  'FSM',
+  'Payment',
+  'PT',
+  'QuickPayLinks',
+  'DSS',
+  'NDSS',
+  'MCollect',
+  'HRMS',
+  'TL',
+  'Receipts',
+  'OBPS',
+  'NOC',
   // "Engagement",
-  "CommonPT",
+  'CommonPT',
+  'Tqm',
 ];
 window.Digit.ComponentRegistryService.setupRegistry({
   ...paymentConfigs,
@@ -86,7 +87,7 @@ const moduleReducers = (initData) => ({
 function App() {
   customizations.forEach((setup) => setup());
   const stateCode =
-    window.globalConfigs?.getConfig("STATE_LEVEL_TENANT_ID") ||
+    window.globalConfigs?.getConfig('STATE_LEVEL_TENANT_ID') ||
     process.env.REACT_APP_STATE_LEVEL_TENANT_ID;
   // const stateCode = "od";
   if (!stateCode) {
